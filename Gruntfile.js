19b712c9aaa0314e89cbb0f6c8507f8f4c0be7ed
James Pamplin <james@jamespamplin.com>
2013-07-28 00:48:47 +0100

    Refactor: changed to use grunt-mocha-phantomjs

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,35 +15,11 @@ module.exports = function(grunt) {
       }
     },
 
-    // Configuration to be run (and then tested).
-    mocha: {
-      src: [ 'spec/runner.html' ],
-      options: {
-        // Bail means if a test fails, grunt will abort. False by default.
-        bail: true,
 
-        // Pipe output console.log from your JS to grunt. False by default.
-        log: true,
-
-        // mocha options
-        // mocha: {
-        //   ignoreLeaks: false,
-        //   grep: 'food'
-        // },
-
-        // Select a Mocha reporter
-        // http://visionmedia.github.com/mocha/#reporters
-        reporter: 'Spec',
-
-        // Indicates whether 'mocha.run()' should be executed in
-        // 'bridge.js'. If you include `mocha.run()` in your html spec,
-        // check if environment is PhantomJS. See example/test/test2.html
-        run: true,
-
-        // Override the timeout of the test (default is 5000)
-        timeout: 10000
+    mocha_phantomjs: {
+      test: {
+        src: [ 'spec/runner.html' ]
       }
-
     }
 
   });
@@ -51,12 +27,12 @@ module.exports = function(grunt) {
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-mocha');
+  grunt.loadNpmTasks('grunt-mocha-phantomjs');
 
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['jshint', 'mocha']);
+  grunt.registerTask('test', ['jshint', 'mocha_phantomjs']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['test']);
